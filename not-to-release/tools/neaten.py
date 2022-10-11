b1be2371bff469f8d7e3b7c1f1979413f53d8bcc
Reece H. Dunn <msclrhd@gmail.com>
2022-10-11 08:49:03 +0100

    docs#517: check lemma, xpos, and features for pronoun 'myself'

--- not-to-release/tools/neaten.py
+++ not-to-release/tools/neaten.py
@@ -594,6 +594,8 @@ pronouns = {
   ("hers","PRP"):{"Gender":"Fem","Number":"Sing","Person":"3","Poss":"Yes","PronType":"Prs","LEMMA":"her"},
   ("its","PRP"):{"Gender":"Neut","Number":"Sing","Person":"3","Poss":"Yes","PronType":"Prs","LEMMA":"its"},
   ("theirs","PRP"):{"Number":"Plur","Person":"3","Poss":"Yes","PronType":"Prs","LEMMA":"their"},
+  # personal, reflexive -- PronType=Prs|Case=Acc|Reflex=Yes
+  ("myself","PRP"):{"Case":"Acc","Number":"Sing","Person":"1","PronType":["Emp","Prs"],"Reflex":"Yes","LEMMA":"myself"},
 }
 
 # See https://universaldependencies.org/en/pos/PRON.html
